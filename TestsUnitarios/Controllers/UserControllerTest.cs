720b1e9a0af11273b66b66838c0ad5339df36a98
Alejandro Pena <apenas@hiberus.com>
2018-08-27 11:56:11 +0200

    was it finished?

--- TestsUnitarios/Controllers/UserControllerTest.cs
+++ TestsUnitarios/Controllers/UserControllerTest.cs
@@ -7,6 +7,7 @@ using System;
 using Xunit;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestsUnitarios.Controllers
 {
@@ -16,26 +17,30 @@ namespace TestsUnitarios.Controllers
         public void GetSocialNetworkList()
         {
             //Arrange
-            var userController = Builder<UserController>.CreateNew().Build();
+            //var optionsBuilder = new DbContextOptionsBuilder<Context>();
+            //optionsBuilder.UseSqlServer("Server=APENAS-LPT\\SQLEXPRESS;Database=PrimeraPrueba;Trusted_Connection=True;MultipleActiveResultSets=true", providerOptions => providerOptions.CommandTimeout(60)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            //var userController = Builder<UserController>.CreateNew().WithFactory(() => new UserController(new Context(optionsBuilder.Options))).Build();
+            var userController = Builder<UserController>.CreateNew().WithFactory(() => new UserController(new Context())).Build();
 
             //Act
             var result = userController.GetSocialNetworkList();
 
             //Assert
-            result.Should().BeOfType<SocialNetworkViewModel>();
+            result.Should().BeOfType<List<SocialNetworkViewModel>>();
+            //result[0].Id.Should().Be(1);
         }
 
         [Fact]
         public void Edit()
         {
             //Arrange
-            var userController = Builder<UserController>.CreateNew().Build();
+            var userController = Builder<UserController>.CreateNew().WithFactory(() => new UserController(new Context())).Build();
 
             //Act
             var result = userController.Edit(1);
 
             //Assert
-            result.Should().BeOfType<ActionResult>();
+            result.Should().BeOfType<ViewResult> ();
         }
     }
 }
